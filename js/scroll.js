b86f01cf3d44175cfc9db23311b15d334e605d21
Jongsun Park <jongsun250@gmail.com>
2021-03-10 23:14:25 +0000

    disable gallary fade effect in mobile view

--- js/scroll.js
+++ js/scroll.js
@@ -84,17 +84,22 @@ const scroll = () => {
     });
   });
 
-  batch(".gallery-item", {
-    interval: 0.1, // time window (in seconds) for batching to occur. The first callback that occurs (of its type) will start the timer, and when it elapses, any other similar callbacks for other targets will be batched into an array and fed to the callback. Default is 0.1
-    batchMax: 5, // maximum batch size (targets)
-    onEnter: (batch) =>
-      gsap.to(batch, { autoAlpha: 1, stagger: 0.5, overwrite: true }),
-    onLeave: (batch) => gsap.set(batch, { autoAlpha: 0, overwrite: true }),
-    onEnterBack: (batch) =>
-      gsap.to(batch, { autoAlpha: 1, stagger: 0.15, overwrite: true }),
-    onLeaveBack: (batch) => gsap.set(batch, { autoAlpha: 0, overwrite: true }),
-    // you can also define things like start, end, etc.
-  });
+  if (window.innerWidth > 700) {
+    batch(".gallery-item", {
+      interval: 0.1, // time window (in seconds) for batching to occur. The first callback that occurs (of its type) will start the timer, and when it elapses, any other similar callbacks for other targets will be batched into an array and fed to the callback. Default is 0.1
+      batchMax: 5, // maximum batch size (targets)
+      onEnter: (batch) =>
+        gsap.to(batch, { autoAlpha: 1, stagger: 0.5, overwrite: true }),
+      onLeave: (batch) => gsap.set(batch, { autoAlpha: 0, overwrite: true }),
+      onEnterBack: (batch) =>
+        gsap.to(batch, { autoAlpha: 1, stagger: 0.15, overwrite: true }),
+      onLeaveBack: (batch) =>
+        gsap.set(batch, { autoAlpha: 0, overwrite: true }),
+      // you can also define things like start, end, etc.
+    });
+  } else {
+    gsap.set(".gallery-item", { autoAlpha: 1 });
+  }
 };
 
 scroll();
